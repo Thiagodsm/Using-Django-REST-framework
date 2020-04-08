fe19f3e6c0ebf0f64957e9707aa269cea776c68c
Thiago Moreira <48686609+Thiagodsm@users.noreply.github.com>
2020-04-08 02:21:36 -0300

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -5,7 +5,7 @@ from .models import Post
 # For example Json format
 
 # The ideia behind the PostSerializers, it's similar to the forms structure of Django.
-# Transformation of the Post model a Json, that contains this two fields: title and description
+# Transformation of the Post model in a Json, that contains this two fields: title and description
 # These fields are returned when something it's requested, through the GET method(Postman)
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
